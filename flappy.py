4063c81093d2a9d710aed4cb78de9a20c65fdf42
Diego <60768726+ZetZed@users.noreply.github.com>
2020-06-11 19:16:42 -0300

    Segundo update

--- flappy.py
+++ flappy.py
@@ -1,8 +1,16 @@
 import pygame
 from pygame.locals import *
 
-SCREEN WIDTH = 400
+SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 800
 
 pygame.init()
-screen = pygame.display.set_mode(())
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+    pygame.display.update()
